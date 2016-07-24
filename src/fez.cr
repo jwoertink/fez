baacc2092abcb109c4b8203d24ce83729eea7c77
Jeremy Woertink <jeremywoertink@gmail.com>
2016-07-24 11:15:31 -0700

    adding option parser

--- src/fez.cr
+++ src/fez.cr
@@ -1,5 +1,13 @@
 require "./fez/*"
+require "option_parser"
 
-module Fez
-  # TODO Put your code here
+application_name = "my_temp_app"
+
+OptionParser.parse! do |parser|
+  parser.banner = "Usage: fez [arguments]"
+  parser.on("-b NAME", "--build=NAME", "Set the NAME of the app") { |name| application_name = name }
+  parser.on("-v", "--version", "Fez version") { puts "Fez v#{Fez::VERSION}" }
+  parser.on("-h", "--help", "Show this help") { puts parser }
 end
+
+puts "building #{application_name}"
