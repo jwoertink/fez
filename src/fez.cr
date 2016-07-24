14271110c874671b1592e65943a9877640582d68
Jeremy Woertink <jeremywoertink@gmail.com>
2016-07-24 11:18:34 -0700

    fix version and help CLI opts so they don't execute the app

--- src/fez.cr
+++ src/fez.cr
@@ -6,8 +6,8 @@ application_name = "my_temp_app"
 OptionParser.parse! do |parser|
   parser.banner = "Usage: fez [arguments]"
   parser.on("-b NAME", "--build=NAME", "Set the NAME of the app") { |name| application_name = name }
-  parser.on("-v", "--version", "Fez version") { puts "Fez v#{Fez::VERSION}" }
-  parser.on("-h", "--help", "Show this help") { puts parser }
+  parser.on("-v", "--version", "Fez version") { puts "Fez v#{Fez::VERSION}"; exit }
+  parser.on("-h", "--help", "Show this help") { puts parser; exit }
 end
 
 puts "building #{application_name}"
