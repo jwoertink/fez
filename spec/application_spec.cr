dcd282b3783b0b1d065f6599b254f7107b1c2121
Jeremy Woertink <jeremywoertink@gmail.com>
2016-07-24 16:56:56 -0700

    not many specs, but they're passing

--- spec/application_spec.cr
+++ spec/application_spec.cr
@@ -1,4 +1,4 @@
-require "spec"
+require "./spec_helper"
 require "../src/fez/application"
 
 describe Fez::Application do
@@ -8,13 +8,19 @@ describe Fez::Application do
       Dir.mkdir_p("/tmp/sample_app")
     end
     Spec.after_each do
-      Dir.rmdir("/tmp/sample_app")
+      FileUtils.rm_r("/tmp/sample_app")
     end
     it "raises an error when the directory exists" do
       expect_raises(Exception) do
         app = Fez::Application.new("sample_app")
         app.build_directory("/tmp")
       end
+    end
+
+    it "creates a new directory in /tmp/sample_app/site" do
+      app = Fez::Application.new("site")
+      app.build_directory("/tmp/sample_app")
+      Dir.exists?("/tmp/sample_app/site").should eq true
     end
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,9 @@
 require "spec"
-require "../src/fez"
+require "file_utils"
+
+# Doing this because the main fez file works more like an executable.
+# Each spec should call it's own file individually.
+# TODO: this throws a weird error :/
+#macro spec_source(filename)
+#  require "./src/fez/#{ {{filename}} }"
+#end
