347a8f4344e55817d35be905b065c16478a6b9a9
Jeremy Woertink <jeremywoertink@gmail.com>
2016-07-25 13:19:08 -0700

    fixing broken spec

--- spec/template_spec.cr
+++ spec/template_spec.cr
@@ -3,8 +3,14 @@ require "../src/fez/template"
 
 describe Fez::Template do
   describe "FILES" do
-    it "is an array with some top level filenames" do
-      Fez::Template::FILES.should be_a Array(String)
+    it "is a hash of filename keys and path values" do
+      Fez::Template::FILES.should be_a Hash(String, String)
+    end
+  end
+
+  describe "FOLDERS" do
+    it "is an array of folder paths" do
+      Fez::Template::FOLDERS.should be_a Array(String)
     end
   end
 end
