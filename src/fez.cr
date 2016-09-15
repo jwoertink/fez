665622e6fcd528deebb6c3d09b7fd52e3139b715
Jeremy Woertink <jeremywoertink@gmail.com>
2016-09-15 11:59:38 -0700

    no more temp app name. -b flag is now required

--- src/fez.cr
+++ src/fez.cr
@@ -3,7 +3,7 @@ require "option_parser"
 require "ecr/macros"
 
 # Default Values
-application_name = "my_temp_app"
+application_name = ""
 application_directory = "."
 
 # Parse the CLI options
@@ -13,6 +13,11 @@ OptionParser.parse! do |parser|
   parser.on("-d DIR", "--directory=DIR", "Set the DIR where the app will be built") { |dir| application_directory = dir }
   parser.on("-v", "--version", "Fez version") { puts "Fez v#{Fez::VERSION}"; exit }
   parser.on("-h", "--help", "Show this help") { puts parser; exit }
+end
+
+if application_name == ""
+  puts "You must specify an application name with the -b flag. View the help menu for more info."
+  exit(1)
 end
 
 # Start the application
