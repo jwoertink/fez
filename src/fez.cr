ddf2baf0f2771290c47b0c3f0881cdf77736f0e3
Jeremy Woertink <jeremywoertink@gmail.com>
2016-10-25 15:20:37 -0700

    help text fix and version bump

--- src/fez.cr
+++ src/fez.cr
@@ -6,7 +6,7 @@ OptionParser.parse! do |parser|
   parser.banner = "Usage: fez [arguments]"
   parser.on("-b NAME", "--build=NAME", "Set the NAME of the app") { |name| Fez::DefaultOptions.application_name = name }
   parser.on("-d DIR", "--directory=DIR", "Set the DIR where the app will be built") { |dir| Fez::DefaultOptions.application_directory = dir }
-  parser.on("-t ENGINE", "--template=ENGINE", "Change the default template engine from Slang") { |engine| Fez::DefaultOptions.template_engine = engine }
+  parser.on("-t ENGINE", "--template=ENGINE", "Change the view template engine. Options are 'slang' or 'ecr'. Default: slang") { |engine| Fez::DefaultOptions.template_engine = engine }
   parser.on("-v", "--version", "Fez version") { puts "Fez v#{Fez::VERSION}"; exit }
   parser.on("-h", "--help", "Show this help") { puts parser; exit }
 end
