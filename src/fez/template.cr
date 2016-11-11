938426d9cc83c4684d15885f6ffb8a581bb91850
Jeremy Woertink <jeremywoertink@gmail.com>
2016-11-10 18:13:13 -0800

    fixing syntax error in generated spec

--- src/fez/template.cr
+++ src/fez/template.cr
@@ -67,7 +67,7 @@ module Fez
         describe "v1 test" do
           it "returns some json" do
             get "/v1/test.json"
-            response.body.should eq {"it" => "works"}.to_json
+            response.body.should eq({"it" => "works"}.to_json)
           end
         end
         CODE
