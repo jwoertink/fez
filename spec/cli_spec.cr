7e72e0b337b5719c3ab6022edea61a0b3b00fe2d
Dru Jensen <drujensen@gmail.com>
2016-11-20 08:10:07 -0800

    fix specs

--- spec/cli_spec.cr
+++ spec/cli_spec.cr
@@ -4,9 +4,9 @@ spec_source "errors", "default_options", "cli"
 describe Fez::CLI do
   describe ".build_project" do
     
-    it "raises a NameError when the name is not given" do
+    it "raises a NameError when the name is invalid" do
       expect_raises Fez::Errors::NameError, "You must specify an application name that begins with a letter using the -b flag. View the help menu for more info." do
-        cli = Fez::CLI.new
+        cli = Fez::CLI.new("123")
         cli.build_project
       end
     end
